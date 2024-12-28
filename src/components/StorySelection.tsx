8020b82f58c3ebddecd3bb70db6efd35aef5131e
Stymax <137451596+Stymax@users.noreply.github.com>
2024-12-28 20:47:47 +0100

    margin botton chevron

--- src/components/StorySelection.tsx
+++ src/components/StorySelection.tsx
@@ -46,7 +46,7 @@ const StorySelection = ({ onBack }: StorySelectionProps) => {
       <div className="relative">
         <button
           onClick={onBack}
-          className="absolute left-0 top-1/2 -translate-y-1/2 text-magical-gold hover:text-magical-gold/80 transition-colors"
+          className="absolute left-0 top-1/2 -translate-y-1/2 text-magical-gold hover:text-magical-gold/80 transition-colors mb-6"
         >
           <ChevronLeft className="w-8 h-8" />
         </button>
