2edacf3a4cf6ec4c5d24e9a3960875ee82f6b65a
Stymax <137451596+Stymax@users.noreply.github.com>
2025-01-09 15:48:26 +0100

    update  content tompouce

--- src/data/tales/tom-pouce/content5.ts
+++ src/data/tales/tom-pouce/content5.ts
@@ -3,42 +3,34 @@ import { TaleSegment } from "@/types/tale";
 export const tomPouceContent5: TaleSegment[] = [
   {
     text: "Il finit par éveiller ses parents, qui accoururent et regardèrent dans la cuisine à travers la serrure. Quand ils virent qu'il y avait un loup, ils s'armèrent, l'homme de sa hache et la femme d'une faux. Reste derrière, dit l'homme à sa femme quand ils entrèrent dans la chambre ; je vais le frapper de ma hache, et si je ne le tue pas du coup, tu lui couperas le ventre. »",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-31.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-41.png"
   },
   {
     text: "Tom Pouce, qui entendait la voix de son père, se mit à crier : « C'est moi, cher père, je suis dans le ventre du loup.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-32.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-42.png"
   },
   {
     text: "— Dieu merci, dit le père plein de joie, notre cher enfant est retrouvé !» Et il ordonna à sa femme de mettre la faux de côté pour ne pas blesser leur fils. Puis, levant sa hache, d'un coup sur la tête il étendit mort le loup, et ensuite, avec un couteau et des ciseaux, il lui ouvrit le ventre et en tira le petit Tom. « Ah! dit-il, que nous avons été inquiets de ton sort !",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-33.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-43.png"
   },
   {
     text: "— Oui, père, j'ai beaucoup couru le monde ; heureusement me voici rendu à la lumière.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-34.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-44.png"
   },
   {
     text: "— Où as-tu donc été ?\n— Ah! père, j'ai été dans un trou de souris, dans la panse d'une vache et dans le ventre d'un loup. Maintenant je reste avec vous.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-35.png"
-  },
-  {
-    text: " Un loup affamé se jeta sur la panse de la vache et l'avala d'un seul coup. Tom Pouce ne perdit pas courage. « Peut-être, pensa-t-il, que ce loup sera traitable. » Et de son ventre, où il était enfermé, il lui cria: « Cher ami loup, je veux t'enseigner un bon repas à faire.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-36.png"
-  },
-  {
-    text: "— Et où cela ? dit le loup.\n— Dans telle et telle maison; tu n'as qu'à te glisser par l'égout de la cuisine, tu trouveras des gâteaux, du lard, des saucisses à bouche que veux-tu. » Et il lui désigna très-exactement la maison de son père.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-37.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-45.png"
   },
   {
     text: "— Et nous ne te revendrions pas pour tout l'or du monde ! » dirent ses parents en l'embrassant et en le serrant contre leur cœur.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-38.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-46.png"
   },
   {
     text: "Ils lui donnèrent à manger et lui firent faire d'autres habits parce que les siens avaient été gâtés pendant son voyage.",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-39.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-47.png"
   },
   {
     text: "Le séjour lui en déplaisait fort, et ce qui compliquait encore sa situation, c'est qu'il descendait toujours de nouveau foin et que l'espace devenait de plus en plus étroit. Enfin, dans sa terreur, Tom s'écria le plus haut qu'il put : « Plus de fourrage ! plus de fourrage ! je n'en veux plus ! »",
-    image: "/lovable-uploads/tom-pouce/tom-pouce-40.png"
+    image: "/lovable-uploads/tom-pouce/tom-pouce-48.png"
   }
 ];
