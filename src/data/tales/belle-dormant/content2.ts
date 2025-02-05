5ab6afba067da51f2bb8de8d5331c729ff681d7a
Stymax <137451596+Stymax@users.noreply.github.com>
2025-02-05 07:37:03 +0100

    update belle dormant

--- src/data/tales/belle-dormant/content2.ts
+++ src/data/tales/belle-dormant/content2.ts
@@ -3,42 +3,42 @@ import { TaleSegment } from "@/types/tale";
 export const belleDormantContent2: TaleSegment[] = [
   {
     text: "Le roi et la reine, qui venaient tout juste de revenir et pénétraient dans la grande salle du palais, s'endormirent. Et avec eux, toute la Cour.\n\nLes chevaux s'endormirent dans leurs écuries, les chiens dans la cour, les pigeons sur le toit, les mouches contre les murs. Même le feu qui brûlait dans l'âtre s'endormit et le rôti s'arrêta de rôtir.",
-    image: "/images/belle-dormant-11.png"
+    image: "/images/belle-dormant/belle-dormant-11.png"
   },
   {
     text: "Le cuisinier, qui était en train de tirer les cheveux du marmiton parce qu'il avait raté un plat, le lâcha et s'endormit. Et le vent cessa de souffler. Nulle feuille ne bougea plus sur les arbres devant le château.\n\nTout autour du palais, une haie d'épines se mit à pousser, qui chaque jour devint plus haute et plus touffue. ",
-    image: "/images/belle-dormant-12.png"
+    image: "/images/belle-dormant/belle-dormant-12.png"
   },
   {
     text: "Bientôt, elle cerna complètement le château, jusqu'à ce qu'on n'en vît plus rien, même pas le drapeau sur le toit.\nDans le pays, la légende de la Belle au Bois Dormant - c'est ainsi que fut nommée la fille du roi - se répandait.",
-    image: "/images/belle-dormant-13.png"
+    image: "/images/belle-dormant/belle-dormant-13.png"
   },
   {
     text: "De temps en temps, des fils de roi s'approchaient du château et tentaient d'y pénétrer à travers l'épaisse muraille d'épines. Mais ils n'y parvenaient pas.\nLes épines se tenaient entre elles, comme par des mains. Les jeunes princes y restaient accrochés, sans pouvoir se détacher et mouraient là, d'une mort cruelle. ",
-    image: "/images/belle-dormant-14.png"
+    image: "/images/belle-dormant/belle-dormant-14.png"
   },
   {
     text: "Au bout de longues, longues années, le fils d'un roi passa par le pays. Un vieillard lui raconta l'histoire de la haie d'épines.\n\nDerrière elle, il devait y avoir un château dans lequel dormait, depuis cent ans, la merveilleuse fille d'un roi, appelée la Belle au Bois Dormant. Avec elle, dormaient le roi, la reine et toute la Cour. ",
-    image: "/images/belle-dormant-15.png"
+    image: "/images/belle-dormant/belle-dormant-15.png"
   },
   {
     text: "Le vieil homme avait aussi appris de son grand-père que de nombreux princes étaient déjà venus qui avaient tenté de forcer la haie d'épines ; mais ils y étaient restés accrochés et y étaient morts d'une triste mort.\n\nLe jeune homme dit alors :\n- Je n'ai peur de rien, je vais y aller. Je veux voir la Belle au Bois Dormant.",
-    image: "/images/belle-dormant-16.png"
+    image: "/images/belle-dormant/belle-dormant-16.png"
   },
   {
     text: "Le bon vieillard voulut l'en empêcher, mais il eut beau faire, le prince ne l'écouta pas.\nOr, les cent années étaient justement écoulées et le jour était venu où la Belle au Bois Dormant devait se réveiller.",
-    image: "/images/belle-dormant-17.png"
+    image: "/images/belle-dormant/belle-dormant-17.png"
   },
   {
     text: "Lorsque le fils du roi s'approcha de la haie d'épines, il vit de magnifiques fleurs qui s'écartaient d'elles-mêmes sur son passage et lui laissaient le chemin. Derrière lui, elles reformaient une haie. Dans le château, il vit les chevaux et les chiens de chasse tachetés qui dormaient. Sur le toit, les pigeons se tenaient la tête sous l'aile.",
-    image: "/images/belle-dormant-18.png"
+    image: "/images/belle-dormant/belle-dormant-18.png"
   },
   {
     text: "Et lorsqu'il pénétra dans le palais, il vit les mouches qui dormaient contre les murs. Le cuisinier, dans la cuisine, avait encore la main levée comme s'il voulait attraper le marmiton et la bonne était assise devant une poule noire qu'elle allait plumer. En haut, sur les marches du trône, le roi et la reine étaient endormis. ",
-    image: "/images/belle-dormant-19.png"
+    image: "/images/belle-dormant/belle-dormant-19.png"
   },
   {
     text: "Le prince poursuivit son chemin et le silence était si profond qu'il entendait son propre souffle.\n\nEnfin, il arriva à la tour et poussa la porte de la petite chambre où dormait la Belle.\n\nElle était là, si jolie qu'il ne put en détourner le regard. Il se pencha sur elle et lui donna un baiser.",
-    image: "/images/belle-dormant-20.png"
+    image: "/images/belle-dormant/belle-dormant-20.png"
   }
 ];
--- src/data/tales/belle-dormant/content3.ts
+++ src/data/tales/belle-dormant/content3.ts
@@ -4,14 +4,14 @@ export const belleDormantContent3: TaleSegment[] = [
   
   {
     text: "Alors, la Belle au Bois Dormant s'éveilla, ouvrit les yeux et le regarda en souriant.\n\nIls sortirent tous deux et le roi s'éveilla à son tour, et la reine, et toute la Cour. Et tout le monde se regardait avec de grands yeux.",
-    image: "/images/belle-dormant-21.png"
+    image: "/images/belle-dormant/belle-dormant-21.png"
   },
   {
     text: "Dans les écuries, les chevaux se dressaient sur leurs pattes et s'ébrouaient ; les chiens de chasse bondirent en remuant la queue. Sur le toit, les pigeons sortirent la tête de sous leurs ailes, regardèrent autour d'eux et s'envolèrent vers la campagne.\nLes mouches, sur les murs, reprirent leur mouvement ; dans la cuisine, le feu s'alluma, flamba et cuisit le repas.",
-    image: "/images/belle-dormant-22.png"
+    image: "/images/belle-dormant/belle-dormant-22.png"
   },
   {
     text: " Le rôti se remit à rissoler ; le cuisinier donna une gifle au marmiton, si fort que celui-ci en cria, et la bonne acheva de plumer la poule. Le mariage du prince et de la Belle au Bois Dormant fut célébré avec un faste exceptionnel. Et ils vécurent heureux jusqu'à leur mort.",
-    image: "/images/belle-dormant-23.png"
+    image: "/images/belle-dormant/belle-dormant-23.png"
   }
 ];
